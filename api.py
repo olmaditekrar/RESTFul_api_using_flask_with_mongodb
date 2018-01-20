2dcffa99f11da0b9b770bd0ed7f7ce503f252f34
Onur Can <olmaditekrar@gmail.com>
2018-01-20 04:05:57 +0300

    -Created a collection and a database in mLab . -Connected the mLab collection and get the all results in that collection with 'GET' method.

--- api.py
+++ api.py
@@ -1,19 +1,32 @@
 from flask import Flask,jsonify,request
-import pymongo
+#from flask.ext.pymongo import PyMongo
 # from flask.ext.mongoalchemy import MongoAlchemy
+import pymongo
 students = [{'name':'Onur Can','surname':'Yucedag'},
-            {'name':'Mertcan', 'surname': 'Cetin'},
-            {'name':'Mihriban','surname':'Avci'}]
-
+           {'name':'Mertcan', 'surname': 'Cetin'},
+           {'name':'Mihriban','surname':'Avci'}]
 app = Flask(__name__)
+# app.config['MONGO_DBNAME'] = 'myexampledb'
+# app.config['MONGO_URI'] = 'mongodb://olmaditekrar:123123124@ds263137.mlab.com:63137/myexampledb'
 
-@app.route("/",methods=['GET'])
-def hello():
-    return jsonify(students)
+uri = 'mongodb://olmaditekrar:123123124@ds263137.mlab.com:63137/myexampledb'
+client = pymongo.MongoClient(uri)
+db = client.get_default_database()
+students = db['students']
+
+
 
 @app.route("/students",methods=['GET'])
 def getAll():
-    return jsonify({'students':students})
+    cursor = students.find()
+    allStudents = []
+    for doc in cursor:
+        allStudents.append({'name': doc['name'] , 'surname': doc['surname']})
+    return jsonify({'students':allStudents})
+
+
+
+
 
 @app.route("/students/<string:name>",methods=['GET'])
 def getOne(name):
@@ -29,7 +42,7 @@ def postStudent():
         return jsonify({'Error' : 'Already have same record.'})
     else:
         students.append(student)
-        return jsonify(students)
+        return jsonify({'students' : students })
 
 @app.route("/students/<string:name>",methods=['PUT'])
 def editOne(name):
@@ -40,7 +53,18 @@ def editOne(name):
         student = student[0]
         student['name'] = request.get_json(force=True).get('name')
         student['surname'] = request.get_json(force=True).get('surname')
-        return jsonify(students)
+        return jsonify({'students' : students })
+
+
+@app.route("/students/<string:name>",methods=['DELETE'])
+def deleteOne(name):
+    student = [student for student in students if student['name'] == name]
+    if student == []:
+        return jsonify({'Error' : 'No record for this name.'})
+    else:
+        student = student[0]
+        students.remove(student)
+        return jsonify({'students' : students })
 
 
 if __name__ == '__main__':
