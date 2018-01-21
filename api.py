7c726335e123769609f2ae1a2154ea48ffac73df
Onur Can <olmaditekrar@gmail.com>
2018-01-21 03:16:53 +0300

    Finished editOne and Post methods. Finished project.

--- api.py
+++ api.py
@@ -1,20 +1,11 @@
 from flask import Flask,jsonify,request
-#from flask.ext.pymongo import PyMongo
-# from flask.ext.mongoalchemy import MongoAlchemy
 import pymongo
-students = [{'name':'Onur Can','surname':'Yucedag'},
-           {'name':'Mertcan', 'surname': 'Cetin'},
-           {'name':'Mihriban','surname':'Avci'}]
-app = Flask(__name__)
-# app.config['MONGO_DBNAME'] = 'myexampledb'
-# app.config['MONGO_URI'] = 'mongodb://olmaditekrar:123123124@ds263137.mlab.com:63137/myexampledb'
 
-uri = 'mongodb://olmaditekrar:123123124@ds263137.mlab.com:63137/myexampledb'
+app = Flask(__name__)
+uri = 'mongodb://<accountName>:<password>@ds263137.mlab.com:63137/myexampledb'
 client = pymongo.MongoClient(uri)
 db = client.get_default_database()
 students = db['students']
-
-
 
 @app.route("/students",methods=['GET'])
 def getAll():
@@ -24,13 +15,11 @@ def getAll():
         allStudents.append({'name': doc['name'] , 'surname': doc['surname']})
     return jsonify({'students':allStudents})
 
-
-
-
-
 @app.route("/students/<string:name>",methods=['GET'])
 def getOne(name):
-    student = [student for student in students if student['name']== name]
+    student = students.find_one({'name' : name})
+    student = { 'name' : student['name'],
+                'surname':student['surname']}
     return jsonify({'student' : student })
 
 @app.route("/students",methods=['POST'])
@@ -38,34 +27,35 @@ def postStudent():
     student = {'name':request.get_json(force=True).get('name'),
                'surname':request.get_json(force=True).get('surname')}
 
-    if student in students:
+    if students.find_one(student):
         return jsonify({'Error' : 'Already have same record.'})
     else:
-        students.append(student)
-        return jsonify({'students' : students })
+        students.insert(student)
+        return getAll()
 
 @app.route("/students/<string:name>",methods=['PUT'])
 def editOne(name):
-    student = [student for student in students if student['name'] == name]
-    if student == []:
-        return jsonify({'Error' : 'No record for this name.'})
-    else:
-        student = student[0]
-        student['name'] = request.get_json(force=True).get('name')
-        student['surname'] = request.get_json(force=True).get('surname')
-        return jsonify({'students' : students })
+    student = students.find_one({'name': name})
+    if student:
+        editedName = request.get_json(force=True).get('name')
+        editedSurname = request.get_json(force=True).get('surname')
 
+        if students.find_one({'name': editedName,'surname':editedSurname}):
+            return jsonify({'Error':'There are already same record.'})
+        else:
+            students.update(student,{'name': editedName,'surname':editedSurname})
+            return getAll()
+    else:
+        return jsonify({'Error': 'No record found for this name.'})
 
 @app.route("/students/<string:name>",methods=['DELETE'])
 def deleteOne(name):
-    student = [student for student in students if student['name'] == name]
-    if student == []:
-        return jsonify({'Error' : 'No record for this name.'})
-    else:
-        student = student[0]
+    student = students.find_one({'name' : name})
+    if student:
         students.remove(student)
-        return jsonify({'students' : students })
-
+        return getAll()
+    else:
+        return jsonify({'Error': 'No record found to delete.'})
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
